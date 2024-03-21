a9d5645c7b8691ff1241fcb4ed123c58883ff448
CAMARGOZRA17 <126689366+CAMARGOZRA17@users.noreply.github.com>
2024-03-21 10:41:23 -0300

    21-03-2024ultimaaula

--- main.js
+++ main.js
@@ -13,3 +13,8 @@ for(let i=0;i <botoes.length;i++){
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date ("2024-05-10T00-00-00");
+
+contadores[0].textContent = tempoObjetivo1;
