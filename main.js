033dae3ac837e86e2a6dbd2b15222d48e55cc705
CAMARGOZRA17 <126689366+CAMARGOZRA17@users.noreply.github.com>
2024-03-21 10:42:52 -0300

    21-03-24

--- main.js
+++ main.js
@@ -16,5 +16,6 @@ for(let i=0;i <botoes.length;i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2024-05-10T00-00-00");
+let tempoAtual = 
 
 contadores[0].textContent = tempoObjetivo1;
